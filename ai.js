70a3556287c26bd69cba293f03394d552bc7231c
Hans Krohn <hanskrohn70@gmail.com>
2021-01-28 15:20:52 -0500

    ai working starting with x

--- ai.js
+++ ai.js
@@ -1,91 +1,73 @@
-function findBestMove(gameBoard, checkWinner){
-    let bestScore = Infinity
-    console.log('calling find best move')
-    let move = {}
-    let id;
-
-    for(let i = 0; i < 3; i++){
-        for(let j = 0; j < 3; j++){
-            if(gameBoard[i][j] === ''){
-                //board wants to minimize
-                gameBoard[i][j] = -1
-                    
-                let score = minimax(gameBoard, checkWinner, true) //therefore next move is maximize
-                gameBoard[i][j] = ''
-                console.log('game score', score)
-                if(score === Infinity){
-                    bestScore = score
-                    move = {i, j}
-                    id = `${i}${j}`
-                    break
-                }
-                if(score < bestScore){
-                    bestScore = score
-                    move = {i, j}
-                    id = `${i}${j}`
-                }
-            }
-        }
-    }
-
-    gameBoard[move.i][move.j] = -1
-    console.log(gameBoard)
-    return id
-}
-
-function minimax(gameBoard, checkWinner, isMaximizing, depth = 0){
-    let winner = checkWinner();
-
-    if(winner !== null){
-        //someone won return winner score
-        return (winner * 100) - depth
-    }
-
-    if(!isMaximizing){
-        //this is ai's goal to minimize the score
-        let bestScore = Infinity
-        for(let i = 0; i < 3; i++){
-            for(let j = 0; j < 3; j++){
-                if(gameBoard[i][j] === ''){
-                    //if a spot is available
-                    gameBoard[i][j] = -1
-                    let score = minimax(gameBoard, checkWinner, !isMaximizing, depth + 1)
-                    gameBoard[i][j] = ''
-
-                    bestScore = Math.min(bestScore, score)
-                }
-            }
-        }
-        return bestScore
-    }else{
-        let bestScore = -Infinity
-        for(let i = 0; i < 3; i++){
-            for(let j = 0; j < 3; j++){
-                if(gameBoard[i][j] === ''){
-                    //if a spot is available
-                    gameBoard[i][j] = 1
-                    let score = minimax(gameBoard, checkWinner, !isMaximizing, depth + 1)
-                    gameBoard[i][j] = ''
-
-                    bestScore = Math.max(bestScore, score)
-                }
-            }
-        }
-        return bestScore
-    }
-}
-
-
-function findAvailableSlots(gameBoard){
+function findAvailableSlots(gamegameBoard){
     let count = 1;
 
     for(let i = 0; i < 3; i++){
         for(let j = 0; j < 3; j++){
-            if(gameBoard[i][j] === ''){
+            if(gamegameBoard[i][j] === ''){
                 count++;
             }
         }
     }
-    // console.log(count, gameBoard)
+    // console.log(count, gamegameBoard)
     return count;
 }
+
+function findBestMove() {
+// AI to make its turn
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+            if (gameBoard[i][j] == '') {
+                gameBoard[i][j] = 1;
+                let score = minimax(gameBoard, 0, false);
+                gameBoard[i][j] = '';
+                if (score > bestScore) {
+                    bestScore = score;
+                    move = { i, j };
+                }
+            }
+        }
+    }
+    gameBoard[move.i][move.j] = 1;
+    return `${move.i}${move.j}`
+}
+
+function minimax(gameBoard, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+        return result*10;
+    }
+
+    if (isMaximizing) {
+        let bestScore = -Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                // Is the spot available?
+                if (gameBoard[i][j] == '') {
+                    gameBoard[i][j] = 1;
+                    let score = minimax(gameBoard, depth + 1, false);
+                    gameBoard[i][j] = '';
+                    bestScore = Math.max(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    } else {
+        let bestScore = Infinity;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                // Is the spot available?
+                if (gameBoard[i][j] == '') {
+                    gameBoard[i][j] = -1;
+                    let score = minimax(gameBoard, depth + 1, true);
+                    gameBoard[i][j] = '';
+                    bestScore = Math.min(score, bestScore);
+                }
+            }
+        }
+        return bestScore;
+    }
+}
+
--- game_logic.js
+++ game_logic.js
@@ -16,41 +16,48 @@ function checkWinner(){
     // if no winner return null
 
     //check horizontal
+    let winner = null;
     for(let i = 0; i < 3; i++){
         if(check3Positions(gameBoard[i][0], gameBoard[i][1], gameBoard[i][2])){
-            gameover = true;
-            return gameBoard[i][0];
+            
+            winner = gameBoard[i][0];
         }
     }
     //check vertical
     for(let i = 0; i < 3; i++){
         if(check3Positions(gameBoard[0][i], gameBoard[1][i], gameBoard[2][i])){
-            gameover = true;
-            return gameBoard[0][i];
+            
+            winner = gameBoard[0][i];
         }
     }
     //check diagonal
     if(check3Positions(gameBoard[0][0], gameBoard[1][1], gameBoard[2][2])){
-        gameover = true;
-        return gameBoard[0][0];
+        
+        winner = gameBoard[0][0];
     }
     
     if(check3Positions(gameBoard[0][2], gameBoard[1][1], gameBoard[2][0])){
-        gameover = true;
-        return gameBoard[0][2];
+        winner = gameBoard[0][2];
     }
     
-    if(available == 0){
-        gameover = true;
-        return 0; //tie
+    let openSpots = 0;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+        if (gameBoard[i][j] == '') {
+            openSpots++;
+        }
+        }
     }
-    
-    //no winner
-    return null;
+
+    if (winner == null && openSpots == 0) {
+        return 0;
+    } else {
+        return winner;
+    }
 }
 
 function check3Positions(a, b, c){
-    return a === b && b === c && a === c && a !== '' && b !== '' && c !== '';
+    return a === b && b === c && a === c && a !== '';
 }
 
 function nextTurn(){
@@ -58,7 +65,7 @@ function nextTurn(){
     if(ai){
         
         const id = findBestMove(gameBoard, checkWinner);
-        draw(id, -1);
+        draw(id, 1);
         return
     }
     player1sTurn = !player1sTurn;
@@ -71,7 +78,7 @@ function selectSquare(i , j){
         return; //spot taken
     }
 
-    const player = player1sTurn ? 1 : -1; //player 1 = 1, player 2 = -1
+    const player = player1sTurn ? -1 : 1; //player 1 = 1, player 2 = -1
     gameBoard[i][j] = player;
     draw(`${i}${j}`, player);
     nextTurn()
@@ -80,9 +87,14 @@ function selectSquare(i , j){
 
 function draw(id, player){
     const div = document.getElementById(id);
+    console.log(div)
     if(player === 1){
         div.innerHTML = '<h4>X</h4>';
     }else{
         div.innerHTML = '<h4>O</h4>';
     }
 }
+setTimeout(() => {
+
+    nextTurn()
+}, 1000)
